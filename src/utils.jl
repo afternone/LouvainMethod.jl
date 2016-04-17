80edc52270d15c19920f64950a4a0966246f870c
afternone <afternone@163.com>
2016-04-18 00:16:59 +0800

    small improvment of tree2partition

--- src/utils.jl
+++ src/utils.jl
@@ -40,3 +40,42 @@ function renumber!(membership::Vector{Int})
         membership[i] = k
     end
 end
+
+function tree2partition(levels::Vector{Vector{Int}}; level=length(levels))
+    (level >= 1 && level <= length(levels)) || error("level must in [1,$(length(levels))]")
+    n = length(levels[1])
+    n2c = collect(1:n)
+    for l=1:level
+        for node=1:n
+            n2c[node] = levels[l][n2c[node]]
+        end
+    end
+    n2c
+end
+
+function tree2partition(levels::Vector{Int}, level_sizes::Vector{Int}; level=length(level_sizes))
+    (level >= 1 && level <= length(level_sizes)) || error("level must in [1,$(length(level_sizes))]")
+    n = level_sizes[1]
+    n2c = collect(1:n)
+    level_start = 0
+    for i=1:level
+        for node=1:n
+            n2c[node] = levels[n2c[node]+level_start]
+        end
+        level_start += level_sizes[i]
+    end
+    n2c
+end
+
+level_size
+tree = [1,1,2,2,3,3,1,1,2,1,1]
+level_size = [6, 3, 2]
+tree2partition(tree, level_size, level=1)
+l1 = [1,1,2,2,3,3]
+l2 = [1,1,2]
+l3 = [1,1]
+levels = Array(Vector{Int},3)
+levels[1] = l1
+levels[2] = l2
+levels[3] = l3
+tree2partition(levels, level=1)
